0f004a07806e7e72bf4633924f3f725db0d83aac
Tejas Agarwal <tejasagarwalbly@gmail.com>
2021-10-16 12:12:21 +0530

    updated lock

--- hosts/delphin/components/user.nix
+++ hosts/delphin/components/user.nix
@@ -10,6 +10,7 @@
 {
   users.users.tejasagarwal = {
 	isNormalUser = true;
+    hashedPassword = "954fff6b071334447f88690c756ec9f964ca207f96f803de500568b8054916d4";
 	extraGroups = [ "wheel" ];
 	shell = pkgs.fish;
   };
