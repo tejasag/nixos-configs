8237c704cf748daa29cc65f4af18c8e941687b50
Tejas Agarwal <tejasagarwalbly@gmail.com>
2021-10-21 22:27:38 +0530

    xmonad config weee

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # `nixos-configs`
 
 My dotfiles and Nix/Nixos Configs.
+
+Everything in the repo is heavily inspired from [Alternateved's Config](httos://github.com/alternateved/nixos-config)
--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -2,15 +2,20 @@
 -- Imports
 ---------------------------------------------
 import XMonad
+-- Layout
+import XMonad.Layout.ThreeColumns
+-- Utils
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
--- Variables
+---------------------------------------------
+-- VARIABLES
+---------------------------------------------
 myFont :: String
 myFont = "xft:Iosevka Nerd Font Mono:style=regular:size=12:antialias=true:hinting=true"
 
 myModMask :: KeyMask
-myModMask = mod4mask
+myModMask = mod4mask -- super/windows key
 
 myTerminal :: String
 myTerminal = "kitty"
@@ -19,11 +24,35 @@ myBrowser :: String
 myBrowser = "brave"
 
 ---------------------------------------------
+-- LAYOUTS
+---------------------------------------------
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    ratio = 1 / 2
+    delta = 3 / 100
+
+---------------------------------------------
+-- KEYBINDS
+---------------------------------------------
+myKeys :: [(String, X ())]
+myKeys =
+  [ -- XMonad
+    ("M-S-r", spawn "xmonad --restart"),
+    -- Terminal
+    ("M-S-<Return>", spawn myTerminal)
+  ]
+
+---------------------------------------------
 -- MAIN
 ---------------------------------------------
 main :: IO ()
 main =
   xmonad $
     def
-      { modMask = myModMask
+      { modMask = myModMask,
+        terminal = myTerminal,
+        layoutHook = myLayout
       }
+      `additionalKeysP` myKeys
