03f65943a168011c4cc394dbbb62c7fa2ec363c3
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-04-24 16:31:43 +0530

    pkgs

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -15,12 +15,21 @@
       gc = "git commit -m $1";
       gp = "git push origin $1";
       commit = "ga . && gc $1";
-      pkgs = "nix- shell --command 'fish' -p";
+      # pkgs = "nix shell --command 'fish' -p";
       clone = "git clone git@github.com/$1/$2.git";
     };
     functions = {
       fish_user_key_bindings.body = "fish_vi_key_bindings";
       nix.body = "IN_NIX_SHELL=impure name=shell command nix $argv";
+      pkgs.body = "
+        set cmd 'nix shell'
+        for i in (string split ' ' $argv)
+          set pkg (string join '' 'nixpkgs#' $i)
+          set cmd (string join ' ' $cmd $pkgs)
+        end
+        printf %s\n $cmd
+        eval $cmd
+      ";
     };
     shellInit = ''
       set -x DIRENV_LOG_FORMAT ""
