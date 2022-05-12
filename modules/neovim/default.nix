ff6f68963762efd0e2b381b20dca48b02e85022b
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-05-12 14:54:43 +0530

    f10

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -35,7 +35,7 @@ colorscheme gruvbox-material
 let g:airline_theme  = 'gruvbox_material'
 
 autocmd filetype cpp nnoremap <f9> :w <bar> !g++ -std=c++14 % -o %:r<cr> 
-autocmd filetype cpp nonoremap <f10> :!%:r<cr>
+autocmd filetype cpp nnoremap <F10> :!%:r<CR>
 set relativenumber number
 set tabstop=4
 set shiftwidth=4
