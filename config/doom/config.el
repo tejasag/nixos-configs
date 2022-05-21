cbb05aadedc877e0263996a7de50ed1d65985d93
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-05-21 12:35:38 +0530

    nord

--- config/doom/config.el
+++ config/doom/config.el
@@ -26,7 +26,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-nord t)
   (setq doom-gruvbox-dark-variant "soft")
   (setq doom-themes-neotree-file-icons t)
 
