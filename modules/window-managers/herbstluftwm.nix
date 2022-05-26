b5d525bb2d8320de55a6d5ee3665741ca7880608
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-05-26 15:35:46 +0530

    added awesome

--- modules/window-managers/herbstluftwm.nix
+++ modules/window-managers/herbstluftwm.nix
@@ -2,6 +2,7 @@
 
 {
   services.xserver.windowManager = {
+    awesome.enable = true;
     herbstluftwm = {
       enable = true;
       configFile = "/home/tejasagarwal/.dotfiles/config/herbstluftwm/autostart";
