b530570c4d36fe5220a8f51d2fb36fa2518e400e
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-05-29 01:44:36 +0530

    removed stuff

--- modules/window-managers/herbstluftwm.nix
+++ modules/window-managers/herbstluftwm.nix
@@ -2,7 +2,6 @@
 
 {
   services.xserver.windowManager = {
-    awesome.enable = true;
     herbstluftwm = {
       enable = true;
       configFile = "/home/tejasagarwal/.dotfiles/config/herbstluftwm/autostart";
