c994dcf52d9030d3be3d2e6ae0912d8a4562f21b
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-06-11 20:04:40 +0530

    updated polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
+kill -9 $(pgrep -f 'polybar') >/dev/null 2>&1
 
-DIR="$HOME/.config/polybar/forest"
-
-# Terminate already running bar instances
-killall -q polybar
+polybar-msg cmd quit >/dev/null 2>&1
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+echo "Launching polybar"
+
+polybar -c ./polybar.ini main 
